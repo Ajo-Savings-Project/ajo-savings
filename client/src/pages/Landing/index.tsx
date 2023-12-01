998db31b38d078351ac80474ef4c94cb203b89b4
Joshua Enyi-Christopher <48376769+KristofaJosh@users.noreply.github.com>
2023-12-01 18:24:30 +0100

    chore: setup eslint (#18)

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -1,18 +1,13 @@
 import Header from './Header'
 import HeroSection from './HeroSection'
-import WhyUseUs from './WhyUseUs'
-import WhatOurUsersSay from './WhatOurUsersSay'
-import WhyUseAjo from './whyUseAjo'
-import Footer from './Footer'
+import HowIsWork from './HowIsWork'
+
 const LandingPage = () => {
   return (
     <>
       <Header />
       <HeroSection />
-      <WhyUseUs />
-      <WhyUseAjo />
-      <WhatOurUsersSay />
-      <Footer />
+      <HowIsWork />
     </>
   )
 }
