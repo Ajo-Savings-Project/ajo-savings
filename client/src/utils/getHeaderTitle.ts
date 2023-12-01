75c246b8db7fbda45f5e176bbe138ef6a4fd8ec8
pelzman <oluwapelumiadeyemo72@gmail.com>
2023-12-01 17:56:41 +0100

    fix getheader

--- client/src/utils/getHeaderTitle.ts
+++ client/src/utils/getHeaderTitle.ts
@@ -1,10 +1,11 @@
-import { HEADER_TITLE } from 'constants'
+import  HEADER_TITLE  from 'constants'
 import { stringToSentenceCase } from 'utils/stringManip.ts'
 
 export const getHeaderTitle = (path: string) => {
   if (path) {
     const paths = path.split('/')
     const page = paths[paths.length - 1]
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
     return `${stringToSentenceCase(page)} | ${HEADER_TITLE}`
   }
   return HEADER_TITLE
