245f6da95665a6f96d672fcdfca8dc8f5650387d
Daniel Jegede <Daniel.Jegede@decagon.dev>
2023-12-01 08:50:26 +0100

    saving branch feat_frequency-select_button

--- client/src/components/compounds/Frquency_Btn/index.tsx
+++ client/src/components/compounds/Frquency_Btn/index.tsx
@@ -1,10 +1,14 @@
 import { Select } from "components"
+import styles from './FrequencyBtn.module.scss'
+import ima from './../../../assets/svg/arrow_drop_down.svg'
 
 const FrequencyBtn = () => {
     return (
         <div>
-            <Select name='frequency' label="frequency" options={[{ label: 'Pick your frquency', value: '', disabled: true }, { label: 'Daily', value: '1' }, { label: 'Weekly', value: '2' }, { label: 'Monthly', value: '3' }, { label: 'Yearly', value: '4' }]} />
-
+            <div className={styles.frequency_container}>
+                <Select className={styles.select_frequency} name='frequency' label="frequency" options={[{ label: 'Pick your frquency', value: '', }, { label: 'Daily', value: '1' }, { label: 'Weekly', value: '2' }, { label: 'Monthly', value: '3' }, { label: 'Yearly', value: '4' }]} />
+                <img className={styles.frequency_dropdown} src={ima} alt="" />
+            </div>
         </div>
     )
 }
