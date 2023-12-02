67e479f15ede4cc34b90265db2a8f44c5af3f47f
Elyzsium <fadiperukayat@gmail.com>
2023-12-02 14:48:49 +0100

    update textarea

--- client/src/components/elements/TextBox/TextArea/index.tsx
+++ client/src/components/elements/TextBox/TextArea/index.tsx
@@ -1,42 +1,19 @@
-import { ChangeEvent, TextareaHTMLAttributes, useState } from 'react'
+import { TextareaHTMLAttributes } from 'react'
 import styles from './textarea.module.scss'
 
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   label: string
 }
 
-interface TextareaState {
-  textValue: string
-}
-
-// eslint-disable-next-line react/prop-types
-const Textarea: React.FC<TextareaProps> = ({ label, ...props }) => {
-  const [state, setState] = useState<TextareaState>({
-    textValue: '',
-  })
-
-  const handleTextareaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setState((prev) => ({
-      ...prev,
-      textValue: event.target.value,
-    }))
-  }
-
+const TextArea = ({ label, ...props }: TextareaProps) => {
   return (
     <div className={styles.container}>
       <label className={styles.Label} htmlFor="textarea">
-        {label}:
+        {label}
       </label>
-      <textarea
-        id="textarea"
-        className={styles.Textarea}
-        value={state.textValue}
-        onChange={handleTextareaChange}
-        {...props}
-      />
-      <p>Current Value: {state.textValue}</p>
+      <textarea {...props} className={styles.textarea} />
     </div>
   )
 }
 
-export default Textarea
+export default TextArea
