d9cb0a3275939afde3d50880b484d86b174ec1c7
Fadipe Rukayat Bukola <40998323+Elyzsium@users.noreply.github.com>
2023-12-02 17:23:25 +0100

    Cu 86bwj0de9/fe   atoms   textarea (#22)
    * update textarea
    
    * update textarea
    
    * update textarea
    
    * update textarea

---
+++ client/src/components/elements/TextBox/TextArea/index.tsx
@@ -0,0 +1,27 @@
+import { TextareaHTMLAttributes } from 'react'
+import styles from './textarea.module.scss'
+import classNames from 'classnames'
+
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string
+}
+
+const TextArea = ({ label, ...props }: TextareaProps) => {
+  return (
+    <div className={styles.inputContainer}>
+      <label
+        className={styles.inputContainerLabel}
+        htmlFor={props.id || label || props.name}
+      >
+        {label}
+      </label>
+      <textarea
+        {...props}
+        id={props.id || label || props.name}
+        className={classNames(styles.inputContainerInput, styles.textarea)}
+      />
+    </div>
+  )
+}
+
+export default TextArea
