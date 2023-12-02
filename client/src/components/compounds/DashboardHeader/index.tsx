6b3820dadfd824225283f98bdf5068e2665b1a4f
Daniel Jegede <Daniel.Jegede@decagon.dev>
2023-12-02 16:11:26 +0100

    fix: move images to parent directory

--- client/src/components/compounds/DashboardHeader/index.tsx
+++ client/src/components/compounds/DashboardHeader/index.tsx
@@ -9,13 +9,8 @@ const DashboardHeader = ({
   className?: string
 }) => {
   return (
-<<<<<<< HEAD
     <header className={classNames('container', styles.header, className)}>
       <button onClick={onClick}>T</button>
-=======
-    <header className={classNames(styles.header, className)}>
-      <button onClick={onClick} >T</button>
->>>>>>> 01d00a2 (feature- created a frequency select button)
       <div>
         <input type="text" />
         <div>A</div>
--- client/src/pages/Landing/WhatOurUsersSay/index.tsx
+++ client/src/pages/Landing/WhatOurUsersSay/index.tsx
@@ -1,64 +1,65 @@
 import { Text } from 'components'
-import Aderemi_pic from '../../../assets/svg/Aderemi.svg'
 import styles from './WhatOurUsersSay.module.scss'
-import polygon from './../../../assets/svg/Polygon1.svg'
+import polygon from './svg/Polygon1.svg'
+import Aderemi_pic from './svg/Aderemi.svg'
+
+const userFeedback = [
+  {
+    name: 'Aderemirekun Ayomide',
+    group: 'Widower Association',
+    picture: Aderemi_pic,
+    feedback:
+      '"Ajo Money has transformed the way I save money. Being part of a thrift group has made it easier to stay motivated and accountable. With Ajó Savings, I\'ve achieved my savings goals faster than ever before!"',
+  },
+  {
+    name: 'Aderemirekun Ayomide',
+    group: 'Widower Association',
+    picture: Aderemi_pic,
+    feedback:
+      '"Ajo Money has transformed the way I save money. Being part of a thrift group has made it easier to stay motivated and accountable. With Ajó Savings, I\'ve achieved my savings goals faster than ever before!"',
+  },
+  {
+    name: 'Aderemirekun Ayomide',
+    group: 'Widower Association',
+    picture: Aderemi_pic,
+    feedback:
+      '"Ajo Money has transformed the way I save money. Being part of a thrift group has made it easier to stay motivated and accountable. With Ajó Savings, I\'ve achieved my savings goals faster than ever before!"',
+  },
+]
 
 const WhatOurUsersSay = () => {
-
-    const user_feedback = [
-        {
-            name: 'Aderemirekun Ayomide',
-            group: 'Widower Association',
-            picture: Aderemi_pic,
-            feedback: '"Ajo Money has transformed the way I save money. Being part of a thrift group has made it easier to stay motivated and accountable. With Ajó Savings, I\'ve achieved my savings goals faster than ever before!"'
-        },
-        {
-            name: 'Aderemirekun Ayomide',
-            group: 'Widower Association',
-            picture: Aderemi_pic,
-            feedback: '"Ajo Money has transformed the way I save money. Being part of a thrift group has made it easier to stay motivated and accountable. With Ajó Savings, I\'ve achieved my savings goals faster than ever before!"'
-        },
-        {
-            name: 'Aderemirekun Ayomide',
-            group: 'Widower Association',
-            picture: Aderemi_pic,
-            feedback: '"Ajo Money has transformed the way I save money. Being part of a thrift group has made it easier to stay motivated and accountable. With Ajó Savings, I\'ve achieved my savings goals faster than ever before!"'
-        },
-    ]
-
-    return (
-        <section className={styles.feedback_container}>
-            <Text
-                content='What our users say?'
-                size={'Subheading'}
-                font={'Inter'}
-                color={'Primary'}
-                className={styles.what_users_say}
-            />
-            <div className={styles.clearfix}>
-                {user_feedback.map((feed, key) => {
-                    return (
-                        <div key={key} className={styles.single_feedback_container}>
-                            <div style={{ position: 'relative' }}>
-                                <Text content={feed.feedback} font={'Inter'} className={styles.single_feedback} />
-                                <img className={styles.polygon} src={polygon} alt="" />
-                            </div>
-                            <div style={{ display: 'flex', gap: '1rem' }}>
-                                <img src={feed.picture} alt={feed.name} />
-                                <div>
-                                    <Text content={feed.name} />
-                                    <Text content={feed.group}
-                                        style={{ color: '#667085' }}
-                                    // color={'Gray'}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
+  return (
+    <section className={styles.feedback_container}>
+      <Text
+        content="What our users say?"
+        size={'Subheading'}
+        color={'Primary'}
+        className={styles.what_users_say}
+      />
+      <div className={styles.clearfix}>
+        {userFeedback.map((feed, key) => {
+          return (
+            <div key={key} className={styles.single_feedback_container}>
+              <div style={{ position: 'relative' }}>
+                <Text
+                  content={feed.feedback}
+                  className={styles.single_feedback}
+                />
+                <img className={styles.polygon} src={polygon} alt="" />
+              </div>
+              <div style={{ display: 'flex', gap: '1rem' }}>
+                <img src={feed.picture} alt={feed.name} />
+                <div>
+                  <Text content={feed.name} />
+                  <Text content={feed.group} style={{ color: '#667085' }} />
+                </div>
+              </div>
             </div>
-        </section>
-    )
+          )
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default WhatOurUsersSay
