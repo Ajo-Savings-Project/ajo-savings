45386141bff13d595b6ec863b4edbd107809d358
kristofajosh <christopherjoshua18@live.com>
2023-12-03 09:29:40 +0100

    style: use accepted values

--- client/src/pages/Landing/WhatOurUsersSay/index.tsx
+++ client/src/pages/Landing/WhatOurUsersSay/index.tsx
@@ -30,33 +30,35 @@ const userFeedback = [
 const WhatOurUsersSay = () => {
   return (
     <section className={styles.feedback_container}>
-      <Text
-        content="What our users say?"
-        size={'Subheading'}
-        color={'Primary'}
-        className={styles.what_users_say}
-      />
-      <div className={styles.clearfix}>
-        {userFeedback.map((feed, key) => {
-          return (
-            <div key={key} className={styles.single_feedback_container}>
-              <div style={{ position: 'relative' }}>
-                <Text
-                  content={feed.feedback}
-                  className={styles.single_feedback}
-                />
-                <img className={styles.polygon} src={polygon} alt="" />
-              </div>
-              <div style={{ display: 'flex', gap: '1rem' }}>
-                <img src={feed.picture} alt={feed.name} />
-                <div>
-                  <Text content={feed.name} />
-                  <Text content={feed.group} style={{ color: '#667085' }} />
+      <div className="container">
+        <Text
+          content="What our users say?"
+          size={'Subheading'}
+          color={'Primary'}
+          className={styles.what_users_say}
+        />
+        <div className={styles.clearfix}>
+          {userFeedback.map((feed, key) => {
+            return (
+              <div key={key} className={styles.single_feedback_container}>
+                <div style={{ position: 'relative' }}>
+                  <Text
+                    content={feed.feedback}
+                    className={styles.single_feedback}
+                  />
+                  <img className={styles.polygon} src={polygon} alt="" />
+                </div>
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                  <img src={feed.picture} alt={feed.name} />
+                  <div>
+                    <Text content={feed.name} />
+                    <Text content={feed.group} color={'Gray'} />
+                  </div>
                 </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </div>
     </section>
   )
