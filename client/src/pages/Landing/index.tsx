940738e20469541d8be9b61e22156157bc9248a6
pelzman <oluwapelumiadeyemo72@gmail.com>
2023-12-04 13:29:18 +0100

    fix: add what our user say

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -1,12 +1,14 @@
 import Header from './Header'
 import HeroSection from './HeroSection'
 import WhyUseUs from './WhyUseUs'
+import WhatOurUsersSay from './WhatOurUsersSay'
 const LandingPage = () => {
   return (
     <>
       <Header />
       <HeroSection />
       <WhyUseUs />
+      <WhatOurUsersSay />
     </>
   )
 }
