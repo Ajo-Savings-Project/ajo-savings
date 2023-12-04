4dffcbb527b95c19912a66e580d2368d90c864d8
pelzman <oluwapelumiadeyemo72@gmail.com>
2023-12-04 13:52:30 +0100

    feat: add whyUseAjo section

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -2,13 +2,16 @@ import Header from './Header'
 import HeroSection from './HeroSection'
 import WhyUseUs from './WhyUseUs'
 import WhatOurUsersSay from './WhatOurUsersSay'
+import WhyUseAjo from './whyUseAjo'
 const LandingPage = () => {
   return (
     <>
       <Header />
       <HeroSection />
       <WhyUseUs />
+      <WhyUseAjo />
       <WhatOurUsersSay />
+   
     </>
   )
 }
