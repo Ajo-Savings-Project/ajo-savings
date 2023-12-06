cbc274248def7860014afe478db09191c5b70062
kristofajosh <christopherjoshua18@live.com>
2023-12-06 09:32:20 +0100

    fix: adjust css naming and remove redundant code
    - adjust classnames
    - UX improvements
    - clean code

--- client/src/pages/Landing/Footer/index.tsx
+++ client/src/pages/Landing/Footer/index.tsx
@@ -1,9 +1,9 @@
 import classNames from 'classnames'
-import styles from './footer.module.scss'
 import { Text } from 'components'
 import Instagram from './images/Instagram.png'
 import Twitter from './images/Twitter.png'
 import Youtube from './images/Youtube.png'
+import styles from './footer.module.scss'
 
 const Footer = () => {
   return (
@@ -11,36 +11,29 @@ const Footer = () => {
       <div className={classNames('container')}>
         <Text
           font={'Bodoni'}
-          className={classNames('app-logo', styles.logo)}
+          className={classNames('app-logo', styles.footerLogo)}
           content={'Ajó Savings'}
-          style={{ color: 'var(--white)' }}
+          color={'White'}
         />
         <div className={styles.footerEnquiry}>
-          <Text
-            content={'For more enquiries:'}
-            className={styles.footerEquiryText}
-          />
-          <Text
-            content={'helpsupport@easylend.com'}
-            className={styles.footerEquirysubText}
-          />
+          <Text content={'For more enquiries:'} color={'White'} />
+          <Text content={'helpsupport@easylend.com'} color={'White'} />
         </div>
-        <div className={styles.footercopyAndSocial}>
+        <div className={styles.footerCopyAndSocials}>
           <div className={styles.footerSocials}>
-            <a href="#" referrerPolicy="no-referrer">
+            <a href="#" referrerPolicy="no-referrer" target="_blank">
               <img src={Instagram} alt="Instagram logo" />
             </a>
-            <a href="#" referrerPolicy="no-referrer">
+            <a href="#" referrerPolicy="no-referrer" target="_blank">
               <img src={Twitter} alt=" Twitter logo" />
             </a>
-            <a href="#" referrerPolicy="no-referrer">
+            <a href="#" rel="noreferrer" target="_blank">
               <img src={Youtube} alt=" Youtube logo" />
             </a>
           </div>
-          <Text
-            content={'© 2023 EasyLend. All rights reserved'}
-            className={styles.copy}
-          />
+          <Text className={styles.copy} color={'White'}>
+            &copy; 2023 EasyLend. All rights reserved
+          </Text>
         </div>
       </div>
     </footer>
