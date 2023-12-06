65bb84e061d9a11902da7cd09c2c2fa7e7db64e5
Daniel Jegede <Daniel.Jegede@decagon.dev>
2023-12-06 22:55:38 +0100

    fix: resolved select button merge conflict

--- client/src/components/elements/TextBox/Select/index.tsx
+++ client/src/components/elements/TextBox/Select/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import { forwardRef, SelectHTMLAttributes } from 'react'
 import styles from './select.module.scss'
+import arrowDropDownSvg from './svg/arrow_drop_down.svg'
 
 interface SelectI extends SelectHTMLAttributes<HTMLSelectElement> {
   label: string
@@ -17,24 +18,30 @@ const Select = forwardRef<HTMLSelectElement, SelectI>(
         >
           {label}
         </label>
-
-        <select
-          ref={ref}
-          name={name}
-          {...props}
-          id={props.id || name || label}
-          className={classNames(
-            styles.select,
-            styles.inputContainerInput,
-            props.className
-          )}
-        >
-          {options.map(({ label, value }) => (
-            <option key={label + value} value={value} disabled={disabled}>
-              {label}
-            </option>
-          ))}
-        </select>
+        <div className={styles.select_icon_select}>
+          <select
+            ref={ref}
+            name={name}
+            {...props}
+            id={props.id || name || label}
+            className={classNames(
+              styles.select,
+              styles.inputContainerInput,
+              props.className
+            )}
+          >
+            {options.map(({ label, value }) => (
+              <option key={label + value} value={value} disabled={disabled}>
+                {label}
+              </option>
+            ))}
+          </select>
+          <img
+            src={arrowDropDownSvg}
+            alt="select button dropdown"
+            className={styles.select_icon}
+          />
+        </div>
       </div>
     )
   }
@@ -43,3 +50,51 @@ const Select = forwardRef<HTMLSelectElement, SelectI>(
 Select.displayName = 'Select'
 
 export default Select
+
+// import classNames from 'classnames'
+// import { SelectHTMLAttributes } from 'react'
+// import styles from './select.module.scss'
+// import arrowDropDownSvg from './svg/arrow_drop_down.svg'
+// interface SelectI extends SelectHTMLAttributes<HTMLSelectElement> {
+//   label: string
+//   options: Array<{ label: string; value: string | number; disabled?: boolean }>
+// }
+
+// const Select = ({ name, label, disabled, options, ...props }: SelectI) => {
+//   return (
+//     <div className={styles.inputContainer}>
+//       <label
+//         className={classNames(styles.inputContainerLabel)}
+//         htmlFor={props.id || name || label}
+//       >
+//         {label}
+//       </label>
+
+//       <div className={styles.select_icon_select}>
+//         <select
+//           name={name}
+//           {...props}
+//           id={props.id || name || label}
+//           className={classNames(
+//             styles.select,
+//             styles.inputContainerInput,
+//             props.className
+//           )}
+//         >
+//           {options.map(({ label, value }) => (
+//             <option key={label + value} value={value} disabled={disabled}>
+//               {label}
+//             </option>
+//           ))}
+//         </select>
+//         <img
+//           src={arrowDropDownSvg}
+//           alt="select button dropdown"
+//           className={styles.select_icon}
+//         />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Select
