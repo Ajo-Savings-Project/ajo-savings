13853f3a6fa12fdeeda54f0c5ba8979a80119b5b
kristofajosh <christopherjoshua18@live.com>
2023-12-08 11:59:55 +0100

    fix: update reset password layout

--- client/src/pages/Auth/ResetPassword/layout.tsx
+++ client/src/pages/Auth/ResetPassword/layout.tsx
@@ -1,16 +1,37 @@
+import classNames from 'classnames'
 import { Helmet } from 'react-helmet'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
+import { HEADER_TITLE } from '../../../appConstants'
+import { Text } from '../../../components'
+import { useAuth } from '../../../contexts'
+import { AuthLayoutFooter } from '../../../layouts/authLayout.tsx'
+import { routes } from '../../../router'
 import { getHeaderTitle } from '../../../utils/getHeaderTitle.ts'
+import styles from './resetPassword.module.scss'
 
 const ResetPasswordLayout = () => {
+  const { isAuth } = useAuth()
+  const location = useLocation()
+
+  if (isAuth) {
+    const prevPath = location.state?.from?.pathname
+    return <Navigate to={prevPath || routes.dashboard.root.path} replace />
+  }
+
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{getHeaderTitle('Reset password')}</title>
-      </Helmet>
-      <div>
-        <Outlet />
+      <Helmet title={getHeaderTitle('Reset password')} />
+      <div className={styles.layout}>
+        <Text
+          className={classNames(styles.layoutLogo, 'app-logo')}
+          font={'Bodoni'}
+          color="Primary"
+          content={HEADER_TITLE}
+        />
+        <div className={styles.layoutContent}>
+          <Outlet />
+        </div>
+        <AuthLayoutFooter className={styles.layoutFooter} />
       </div>
     </>
   )
