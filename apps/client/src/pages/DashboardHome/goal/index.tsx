03be504dd769eb65f0c0542496a839c8191edce0
Elyzsium <fadiperukayat@gmail.com>
2023-12-19 07:15:04 +0100

    feat: CU-86bwmcp3c/Build-my-goal-component

--- apps/client/src/pages/DashboardHome/goal/index.tsx
+++ apps/client/src/pages/DashboardHome/goal/index.tsx
@@ -1,34 +1,32 @@
-
 import { Link } from "react-router-dom";
 import Travel from "./images/travel.svg";
 import Home from "./images/home.svg";
-import Car from "./images/car.svg"
-import styles from "./goal.module.scss"
+import Car from "./images/car.svg";
+import styles from "./goal.module.scss";
 import { Card, Text } from "components";
-
-
+import ProgressBar from "./ProgressBar";
 
 export const data = [
   {
     id: "1",
     image: Travel,
     target: "Travel",
-    amount_saved: "₦ 5,000,000.00",
-    target_amount: "₦ 10,000,000.00",
+    amount_saved: 5_000_000.00,
+    target_amount: 10_000_000.00,
   },
   {
     id: "2",
     image: Home,
     target: "Dream Home",
-    amount_saved: "₦ 10,000,000.00",
-    target_amount: "₦ 40,000,000.00",
+    amount_saved: 10_000_000.00,
+    target_amount: 40_000_000.00,
   },
   {
     id: "3",
     image: Car,
     target: "Dream Car",
-    amount_saved: "₦ 1,000,000.00",
-    target_amount: "₦ 5,000,000.00",
+    amount_saved: 1_000_000.00,
+    target_amount: 5_000_000.00,
   },
 ];
 
@@ -44,28 +42,30 @@ const MyGoals = () => {
       {data.map((target, index) => (
         <Card key={index} className={styles.goalSection}>
           <div className={styles.infoSection}>
-          <div className={styles.goal}>
-            <img className={styles.image} alt="image" src={target.image} />
-            <div className={styles.info}>
-              <Text size={'Small'} className={styles.target} content={target.target}  />
-              <div className={styles.amount}>
-              <Text size={'Small'} className={styles.saved} content={target.amount_saved} />
-              <Text size={'Small'} className={styles.targetAmount} content={`/ ${target.target_amount}`} />
+            <div className={styles.goal}>
+              <img className={styles.image} alt="image" src={target.image} />
+              <div className={styles.info}>
+                <Text size={'Small'} className={styles.Target} content={target.target} />
+                <div className={styles.amount}>
+                  <Text size={'Small'} className={styles.saved} content={new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN' }).format(target.amount_saved)} />
+                  <Text size={'Small'} className={styles.targetAmount} content={`/${new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN'}).format(target.target_amount)}`} />
+                </div>
+              </div>
             </div>
+            <div className={styles.percentage}>
+              {Math.round((parseFloat(target.amount_saved.toString().replace("₦", "").replace(",", "")) / parseFloat(target.target_amount.toString().replace("₦", "").replace(",", ""))) * 100)}%
             </div>
           </div>
-          <div className={styles.percentage_text}>
-           {Math.round((parseFloat(target.amount_saved.replace("₦", "").replace(",", "")) / parseFloat(target.target_amount.replace("₦", "").replace(",", ""))) * 100)}%
-          </div>  
+          <div className={styles.dashedLinesContainer}>
+            {[...Array(10)].map((_, index) => (
+              <div
+                key={index}
+                className={
+                  index < 5 ? styles.dashedLine : styles.dashedLineLeft
+                }
+              ></div>
+            ))}
           </div>
-          <div className={styles.dash}></div>
-          {/* <div className="progress-bar-container">
-            <ProgressBar 
-            className={styles.progressBar} 
-            
-            // percentage={((target.amount_saved / target.target_amount) * 100)} 
-            />
-          </div>  */}
         </Card>
       ))}
     </div>
