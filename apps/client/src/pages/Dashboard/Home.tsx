7ae2d8331fe90d839f5d690fe6130057515eaaf1
Daniel Jegede <131445852+Dallat65@users.noreply.github.com>
2023-12-19 09:32:03 +0100

    feat:designed upcoming activites as a compound in dashboard home page (#58)
    * Fix/login message (#71)
    
    * patch: add groups pages
    
    * fix: login message
    
    * fix:redesigned image to have border radius
    
    ---------
    
    Co-authored-by: Joshua Enyi-Christopher <48376769+KristofaJosh@users.noreply.github.com>

--- apps/client/src/pages/Dashboard/Home.tsx
+++ apps/client/src/pages/Dashboard/Home.tsx
@@ -1,5 +1,6 @@
 import { KYCSetup, Text } from 'components'
 import { useAuth } from '../../contexts'
+import UpcomingActivities from 'components/compounds/UpcomingActivities/UpcomingActivities'
 
 const HomePage = () => {
   const v = useAuth()
@@ -13,6 +14,7 @@ const HomePage = () => {
       <Text content={`Welcome back ${firstName} ${lastName},`.trim()} />
       <Text>Date</Text>
       <div>content goes here</div>
+      <UpcomingActivities />
     </>
   )
 }
