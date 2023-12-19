05bf61569ff6bcef5e7da0e4c47430bb18ad65f9
pelzman <oluwapelumiadeyemo72@gmail.com>
2023-12-19 10:58:54 +0100

    fix: merge conflict

--- apps/client/src/pages/Dashboard/Home.tsx
+++ apps/client/src/pages/Dashboard/Home.tsx
@@ -1,53 +1,18 @@
-import { KYCSetup,Button, Text } from 'components'
+import { KYCSetup, Text } from 'components'
+import { useAuth } from '../../contexts'
 import UpcomingActivities from 'components/compounds/UpcomingActivities/UpcomingActivities'
-import viteLogo from '/vite.svg?url'
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import Logo from '../../assets/react.svg?react'
+
 import ChartWithUpcoming from './chartAndUpcoming'
 const HomePage = () => {
-  const [count, setCount] = useState(0)
+  const { firstName, lastName } = useAuth()
 
   return (
     <>
       <KYCSetup />
-      <Text content={`Welcome back},`.trim()} />
+      <Text content={`Welcome back ${firstName} ${lastName},`.trim()} />
       <Text>Date</Text>
       <div>content goes here</div>
       <UpcomingActivities />
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button
-          text={`count is ${count}`}
-          onClick={() => setCount((count) => count + 1)}
-        />
-        <Button
-          kind={'rounded'}
-          text={`count is ${count}`}
-          onClick={() => setCount((count) => count + 1)}
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <Logo width={70} height={70} />
-      <Text
-        className="read-the-docs"
-        content={'Click on the Vite and React logos to learn more'}
-      />
-      <Text
-        level={6}
-        className="read-the-docs"
-        content={'Click on the Vite and React logos to learn more'}
-      />
       <ChartWithUpcoming />
     </>
   )
