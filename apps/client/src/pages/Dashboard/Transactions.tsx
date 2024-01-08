51a390c21ad8b9d3929e3c5d6f5e1c8f22dbb810
Daniel Jegede <Daniel.Jegede@decagon.dev>
2024-01-08 21:11:15 +0100

    feat- designed transaction page

--- apps/client/src/pages/Dashboard/Transactions.tsx
+++ apps/client/src/pages/Dashboard/Transactions.tsx
@@ -1,5 +1,28 @@
+import { Text } from 'components'
+import {
+  TodayResponsiveTable,
+  YesterdayResponsiveTable,
+} from 'components/compounds/TransactionDetails/ResponsiveTable'
+
 const TransactionsPage = () => {
-  return <div>Transaction Page</div>
+  return (
+    <div style={{ maxWidth: '1000px' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Text content="Transactions" size="Subtext" />
+        <div style={{ color: '#2F80ED' }}>
+          <Text content="Clear all" color="none" size="Small" />
+        </div>
+      </div>
+      <TodayResponsiveTable />
+      <YesterdayResponsiveTable />
+    </div>
+  )
 }
 
 export default TransactionsPage
