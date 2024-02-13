170d549392f13d9c3019668c807a75537dbbe9d0
kristofajosh <christopherjoshua18@live.com>
2024-02-13 16:57:28 +0100

    update: keep dev session longer

--- apps/server/src/constants/index.ts
+++ apps/server/src/constants/index.ts
@@ -1,8 +1,10 @@
+import Env from '../config/env'
+
 export * from './httpStatusCode'
 
 export const ACCESS_TOKEN = 'sessions.accessToken.ajo'
 export const REFRESH_TOKEN = 'sessions.ajo'
-export const JWT_ACCESS_TOKEN_EXPIRATION_TIME = '5m'
-export const JWT_REFRESH_TOKEN_EXPIRATION_TIME = '30m'
+export const JWT_ACCESS_TOKEN_EXPIRATION_TIME = Env.IS_PROD ? '5m' : '35m'
+export const JWT_REFRESH_TOKEN_EXPIRATION_TIME = Env.IS_PROD ? '15m' : '40m'
 export const JWT_EXPIRATION_STATUS_CODE = 'JWT: Expired'
 export const JWT_INVALID_STATUS_CODE = 'JWT: Invalid'
