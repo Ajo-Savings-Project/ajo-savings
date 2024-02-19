e912e800fef81bc26a5e8c5273febc2a723b6915
AnosikeChidinma <anosikechidinma@gmail.com>
2024-02-19 17:29:20 +0100

    Feat-Profile Account Snippet

--- apps/client/src/components/compounds/DashboardHeader/index.tsx
+++ apps/client/src/components/compounds/DashboardHeader/index.tsx
@@ -1,5 +1,10 @@
+import { useState } from 'react'
+import { Popover } from 'react-tiny-popover'
 import classNames from 'classnames'
 import styles from './dashboardheader.module.scss'
+import ProfilePicture from './DefaultAjo-dp.svg?react'
+import SearchIcon from './search-altsearchIcon.svg?react'
+import { Input, Text } from 'components'
 
 const DashboardHeader = ({
   onClick,
@@ -8,12 +13,53 @@ const DashboardHeader = ({
   onClick: () => void
   className?: string
 }) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+
   return (
     <header className={classNames('container', styles.header, className)}>
       <button onClick={onClick}>T</button>
       <div>
-        <input type="text" />
-        <div>A</div>
+        <div className={styles.headerSearchInput}>
+          <SearchIcon className={styles.headerSearchIcon} />
+          <Input
+            className={styles.headerSearchPlaceholder}
+            type="text"
+            label={''}
+            placeholder={'Search'}
+          />
+        </div>
+        <Popover
+          isOpen={isPopoverOpen}
+          positions={['bottom']}
+          onClickOutside={() => setIsPopoverOpen(false)}
+          content={
+            <div
+              style={{
+                backgroundColor: 'white',
+                margin: '5px',
+                textAlign: 'start',
+                color: 'black',
+                padding: '20px',
+              }}
+            >
+              <Text size={'Small'} style={{ marginBottom: '15px' }}>
+                Upload Picture
+              </Text>
+              <Text size={'Small'}>Change Password</Text>
+            </div>
+          }
+        >
+          <div className={styles.headerImage}>
+            <ProfilePicture onClick={() => setIsPopoverOpen(!isPopoverOpen)} />
+            <Text
+              content={'Deborah'}
+              size={'Small'}
+              color={'Gray'}
+              className={styles.headerText}
+              onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+            />
+          </div>
+        </Popover>
       </div>
     </header>
   )
