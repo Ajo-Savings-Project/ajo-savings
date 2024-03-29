dc15d1cbe3b84bb1d5025ff1f88ff30f61607cb4
AnosikeChidinma <anosikechidinma@gmail.com>
2024-03-29 22:10:26 +0100

    resolved coflict

--- apps/client/src/components/compounds/SetTarget/request.ts
+++ apps/client/src/components/compounds/SetTarget/request.ts
@@ -1,9 +1,13 @@
 import { z, object } from 'zod'
+import request from 'api'
+import { useMutation } from 'react-query'
 
 export const SetTargetSchema = z.object({
-  target: z.string().min(1, 'Target is required'),
+  name: z.string().min(1, 'Target is required'),
   targetAmount: z.string().min(1, 'Total amount is required'),
   frequency: z.string().min(1, 'Frequency is required'),
+  category: z.string().min(1, 'Category is required'),
+
   startDate: z
     .string()
     .refine((date) => date.length > 0, { message: 'Start Date is required' }),
@@ -15,7 +19,24 @@ export const SetTargetSchema = z.object({
 export const SetTargetResponseSchema = z.object({
   success: z.boolean(),
   data: object({
-    targetId: z.string(),
-    message: z.string(),
+    name: z.string(),
+    targetAmount: z.string(),
+    frequency: z.string(),
+    category: z.string(),
+    startDate: z.string(),
+    withdrawalDate: z.string(),
   }),
 })
+
+export const useCreateTargetMutation = () => {
+  return useMutation({
+    mutationFn: async (data: z.infer<typeof SetTargetSchema>) => {
+      const res = await request.post('/targets/createTarget', {
+        ...data,
+        targetAmount: Number(data.targetAmount),
+      })
+      const result = SetTargetResponseSchema.safeParse(res.data)
+      if (result.success) return result
+    },
+  })
+}
